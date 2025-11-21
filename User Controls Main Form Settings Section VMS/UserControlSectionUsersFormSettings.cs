6162ef3cd9487ae4557687047b4f9f43ef587f10
Ahmed Jehad <alanturing404@gmail.com>
2025-11-21 10:39:44 +0200

    Added a new folder to organize all settings-related UserControls and added the main settings UserControl.

---
+++ User Controls Main Form Settings Section VMS/UserControlSectionUsersFormSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visitor_Management_System.User_Controls_Main_Form_Settings_Section_VMS
+{
+    public partial class UserControlSectionUsersFormSettings : UserControl
+    {
+        public UserControlSectionUsersFormSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
